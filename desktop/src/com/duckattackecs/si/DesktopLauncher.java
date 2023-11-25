d9db3a396affa2408cb4159312e314ab33e43c7b
Matic Lukežič <maticlukezic007@gmail.com>
2023-11-25 19:31:07 +0100

    Initial commit

---
+++ desktop/src/com/duckattackecs/si/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.duckattackecs.si;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.duckattackecs.si.DuckAttackECS;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("DuckAttackECS");
+		new Lwjgl3Application(new DuckAttackECS(), config);
+	}
+}
